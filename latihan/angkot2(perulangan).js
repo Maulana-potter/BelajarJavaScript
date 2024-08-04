1bf778708232482f35694c3f086313913a0cee59
Maulana-potter <nazar123nazar122@gmail.com>
2024-08-04 21:25:37 +0700

    belajar perulangan

---
+++ latihan/angkot2(perulangan).js
@@ -0,0 +1,13 @@
+var noAngkot=1
+var angkotBeroperasi = 6
+var jlmAngkot =10
+ 
+while (noAngkot <= angkotBeroperasi ){
+    console.log("angkot No " + noAngkot + "beroperasi dengan baik")
+    noAngkot++
+}
+
+ for(var noAngkot = angkotBeroperasi + 1 ; noAngkot <= jlmAngkot ; noAngkot++){
+    console.log("angkot No " + noAngkot + " Tidak beroperasi dengan baik")
+
+ }

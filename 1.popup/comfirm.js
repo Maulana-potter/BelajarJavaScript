26e0a5feb50cd9443b90b49fba14d11155518a17
Maulana-potter <nazar123nazar122@gmail.com>
2024-08-04 20:25:48 +0700

    belajar popup-box

---
+++ 1.popup/comfirm.js
@@ -0,0 +1,8 @@
+var tes = confirm ('apakah kamu mau duit gratis?')
+
+if (tes === true){
+    a = prompt ("masuka nama jika mau duet : ")
+    alert("selamat kamau mendapatkan duet " +a)
+}else{
+    alert("okelah kalau ga mau")
+}
